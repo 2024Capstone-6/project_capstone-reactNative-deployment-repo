c14df4fd85383e2d519f8ee610de375478147cab
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2025-04-15 19:50:06 +0900

    ✨ feat: Implement initial layout for user page

--- app/(tabs)/user/index.tsx
+++ app/(tabs)/user/index.tsx
@@ -23,9 +23,29 @@ export default function UserScreen() {
   };
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <ThemedText type="title">User</ThemedText>
-
+    <View className="flex-1">
+      <View className="top-0 w-full h-[35%] z-10 rounded-b-[50%] bg-[#ff6b6b] relative">
+        {/* 프로필 이미지 */}
+        <View className="absolute bottom-[-70px] left-1/2 -translate-x-1/2 w-[150px] h-[150px] rounded-full bg-white shadow-md"></View>
+      </View>
+      <View className="items-center mt-20">
+        <ThemedText className="text-lg font-bold">dlatth</ThemedText>
+        <ThemedText className="text-gray-500">test02@gmail.com</ThemedText>
+      </View>
+      <View className="items-start m-4">
+        <ThemedText className="text-lg text-[#ff6b6b] font-bold mb-2">단어</ThemedText>
+        <View className="flex-row w-full h-[100px] mb-4">
+          <TouchableOpacity style={styles.levelCard}>
+            <ThemedText type="default">중간고사</ThemedText>
+          </TouchableOpacity>
+        </View>
+        <ThemedText className="text-lg text-[#ff6b6b] font-bold mb-2">문법</ThemedText>
+        <View className="flex-row w-full h-[100px] mb-4">
+          <TouchableOpacity style={styles.levelCard}>
+            <ThemedText type="default">JPT</ThemedText>
+          </TouchableOpacity>
+        </View>
+      </View>
       <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
         <ThemedText style={styles.logoutText}>Logout</ThemedText>
       </TouchableOpacity>
@@ -34,14 +54,35 @@ export default function UserScreen() {
 }
 
 const styles = StyleSheet.create({
+  levelCard: {
+    aspectRatio: 1,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ff6b6b',
+    borderRadius: 10,
+    marginRight: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 3, height: 1.5 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1.5,
+    elevation: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  levelText: {
+    textAlign: 'center',
+  },
   logoutButton: {
+    width: '40%',
+    alignSelf: 'center',
     backgroundColor: Colors.tint,
-    paddingVertical: 12,
+    paddingVertical: 4,
     paddingHorizontal: 24,
     borderRadius: 8,
     marginTop: 20,
   },
   logoutText: {
+    textAlign: 'center',
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
